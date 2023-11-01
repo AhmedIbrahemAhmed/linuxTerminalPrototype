922a7e6452e4b25adc78340e3aa5ff838997017e
MarwanTarik <marwantarik64@gmail.com>
2023-11-01 08:17:51 +0200

    init

---
+++ src/Parser.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Parser {
+  String commandName;
+  String[] args;
+  String[] flags;
+  public Parser() {
+    this.commandName = "";
+    this.args = new String[10];
+    this.flags = new String[10];
+  }
+  public boolean parse(String input) {
+    String[] array;
+    array = input.split(" ");
+    this.commandName = array[0];
+    for (int i=1; i < array.length; i++) {
+      if (array[i].charAt(0) == '-') {
+        flags[i] = array[i];
+      } else {
+        args[i] = array[i];
+      }
+    }
+    return !Objects.equals(commandName, "");
+  }
+  public String getCommandName() {
+    return commandName;
+  }
+  public String[] getArgs() {
+    return args;
+  }
+  public String[] getFlags() {
+    return flags;
+  }
+}
---
+++ src/Terminal.java
@@ -0,0 +1,18 @@
+public class Terminal {
+  Parser parser;
+  public void echo() {}
+  public String pwd() {}
+  public void cd(String[] args) {}
+  public void ls() {}
+  public void mkdir() {}
+  public void rmdir() {}
+  public void touch() {}
+  public void cp() {}
+  public void rm() {}
+  public void cat() {}
+  public void history() {}
+
+  // TODO cp -r, ls -r
+  public void chooseCommandAction(){}
+  public static void main(String[] args){}
+}
